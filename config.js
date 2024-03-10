6cc62d42fba35b4a91a0363cbbd1fcdf02f9bd3d
LenniLenLen <162921725+LenniLenLen@users.noreply.github.com>
2024-03-10 21:50:34 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  OWNER_IDS: [""], // Bot owner ID's
+  OWNER_IDS: ["1077503008696446977"], // Bot owner ID's
   SUPPORT_SERVER: "", // Your bot support server
   PREFIX_COMMANDS: {
     ENABLED: true, // Enable/Disable prefix commands
